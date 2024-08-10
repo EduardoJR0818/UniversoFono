29c03e49b9cd09ae856d255d54c7e2493f2ec289
EduardoJR08181 <edujura990818@gmail.com>
2024-08-09 20:20:46 -0600

    tercero

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://eduardojr0818.github.io/UniversoFono/",
+  base: "/UniversoFono/",
 });
